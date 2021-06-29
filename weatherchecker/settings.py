fe9cdb6f94a2ef148ca4fbbbd9a6727f20514c78
= <>
2021-06-29 13:00:47 -0500

    Fix problem in settings.py

--- weatherchecker/settings.py
+++ weatherchecker/settings.py
@@ -98,14 +98,12 @@ if DEVELOPMENT_MODE is True:
         }
     }
 
-"""
 elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
